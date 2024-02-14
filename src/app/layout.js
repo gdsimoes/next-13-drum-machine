13a14b2d4da1864443c32c60dc4f67cf6a342bac
Guilherme Simoes <gds.simoes@gmail.com>
2024-02-14 10:27:06 -0500

    Add solution without using context

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import Header from '../components/Header';
-
-import './styles.css';
+import "./styles.css";
 
 function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <Header />
-        {children}
-        <footer>
-          <img src="/ie-badge.gif" width={100} />
-          <span>Thanks for visiting!</span>
-        </footer>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                {children}
+                <footer>
+                    <img src="/ie-badge.gif" width={100} />
+                    <span>Thanks for visiting!</span>
+                </footer>
+            </body>
+        </html>
+    );
 }
 
 export default RootLayout;
--- src/components/DrumMachine/DrumMachine.js
+++ src/components/DrumMachine/DrumMachine.js
@@ -1,46 +1,38 @@
-'use client';
-import React from 'react';
-import useSound from 'use-sound';
+"use client";
+import React from "react";
+import useSound from "use-sound";
 
-import styles from './DrumMachine.module.css';
+import styles from "./DrumMachine.module.css";
 
-const SOUND_SRC = '/909-drums.mp3';
+const SOUND_SRC = "/909-drums.mp3";
 
-function DrumMachine() {
-  const [play] = useSound(SOUND_SRC, {
-    sprite: {
-      kick: [0, 350],
-      hihat: [374, 160],
-      snare: [666, 290],
-      cowbell: [968, 200],
-    },
-    soundEnabled: true,
-  });
+function DrumMachine({ soundEnabled }) {
+    const [play] = useSound(SOUND_SRC, {
+        sprite: {
+            kick: [0, 350],
+            hihat: [374, 160],
+            snare: [666, 290],
+            cowbell: [968, 200],
+        },
+        soundEnabled: true,
+    });
 
-  return (
-    <div className={styles.wrapper}>
-      <button
-        onClick={() => play({ id: 'kick' })}
-      >
-        Kick
-      </button>
-      <button
-        onClick={() => play({ id: 'hihat' })}
-      >
-        Hat
-      </button>
-      <button
-        onClick={() => play({ id: 'snare' })}
-      >
-        Snare
-      </button>
-      <button
-        onClick={() => play({ id: 'cowbell' })}
-      >
-        Cowbell
-      </button>
-    </div>
-  );
+    function playFunctionCreator(id) {
+        return () => {
+            if (soundEnabled) {
+                play({ id });
+            }
+        };
+    }
+
+    return (
+        <div className={styles.wrapper}>
+            <button onClick={playFunctionCreator("kick")}>Kick</button>
+            <button onClick={playFunctionCreator("hihat")}>Hat</button>
+            <button onClick={playFunctionCreator("snare")}>Snare</button>
+            <button onClick={playFunctionCreator("cowbell")}>Cowbell</button>
+        </div>
+    );
 }
 
 export default DrumMachine;
